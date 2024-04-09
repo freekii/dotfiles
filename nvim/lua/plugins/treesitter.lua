a91b6ed418a70b7158c48306786da187b6be8dd7
freekii <matthewdylanfreeman@gmail.com>
2024-04-09 20:13:54 +1000

    Setup autopairs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,32 @@
-return { 
-  "nvim-treesitter/nvim-treesitter", 
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },  
-    })
-  end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+
+		local npairs = require("nvim-autopairs")
+		local Rule = require("nvim-autopairs.rule")
+
+		npairs.setup({
+			check_ts = true,
+			ts_config = {
+				lua = { "string" }, -- it will not add a pair on that treesitter node
+				javascript = { "template_string" },
+				java = false, -- don't check treesitter on java
+			},
+		})
+
+		local ts_conds = require("nvim-autopairs.ts-conds")
+
+		-- press % => %% only while inside a comment or string
+		npairs.add_rules({
+			Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
+			Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
+		})
+	end,
 }
