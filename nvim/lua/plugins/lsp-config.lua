a871d33b88f4ac8121aaac011d86acfc683ceb12
Matthew Freeman <mfreeman@atlassian.com>
2024-04-16 15:30:58 +1000

    enable kotlin and java

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -33,11 +33,18 @@ return {
 			lspconfig.graphql.setup({
 				capabilities = capabilities,
 			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.kotlin_language_server.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
 		end,
 	},
