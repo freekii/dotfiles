ebfb08e3f9f1264c4ba8fad708b109caac63f652
Matthew Freeman <mfreeman@atlassian.com>
2024-04-22 14:19:27 +1000

    turn on git line blame

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,8 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		local gitsigns = require("gitsigns")
-		gitsigns.setup()
+		gitsigns.setup({
+			current_line_blame = true,
+		})
 	end,
 }
