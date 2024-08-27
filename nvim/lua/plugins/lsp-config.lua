0be1d2858853a869aa4ddcabd7cd2cbc8d4b8454
Matthew Freeman <mfreeman@atlassian.com>
2024-08-27 14:17:09 +1000

    use telescope for references

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -48,7 +48,7 @@ return {
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, {})
 			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
 		end,
 	},
