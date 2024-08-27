a0f477a37470c2c49ded77dc880c514f9baf2a68
Matthew Freeman <mfreeman@atlassian.com>
2024-08-27 14:43:28 +1000

    also use telescope for definitions and implementations

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -47,9 +47,9 @@ return {
 			vim.keymap.set("n", "I", vim.diagnostic.open_float, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gd", require("telescope.builtin").lsp_definitions, {})
 			vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, {})
-			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "<leader>gi", require("telescope.builtin").lsp_implementations, {})
 		end,
 	},
 }
