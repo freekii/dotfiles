56627e470fa334f47f1d25c991fbd124fa449290
Matthew Freeman <mfreeman@atlassian.com>
2024-09-20 15:30:22 +1000

    find hidden files with telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,6 +7,14 @@ return {
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>o", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>ff", builtin.live_grep, {})
+
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+			})
 		end,
 	},
 	{
