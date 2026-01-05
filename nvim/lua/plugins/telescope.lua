f73b52ce14f168ca91671baa625573549ae18bcb
Matthew Freeman <mfreeman@atlassian.com>
2026-01-06 09:57:30 +1100

    matches first in telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,17 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
+		tag = "v0.2.1",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("telescope").setup({
 				defaults = {
 					layout_strategy = "vertical",
+					path_display = {
+						filename_first = {
+							reverse_directories = false,
+						},
+					},
 				},
 				pickers = {
 					find_files = {
